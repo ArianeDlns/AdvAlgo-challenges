9c42283dee5980bcc93632b1a8212783bd1fe449
ArianeDlns <ariane.dalens@student.ecp.fr>
2021-10-14 17:59:33 +0200

    [dominant] first draft

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Solve algorithm challenges for the Advances Algorithm course @CentraleSupélec
 
 ## First challenge:
-:world_map: Dominant - on [codalab](https://competitions.codalab.org/competitions/35662?secret_key=d486de59-a936-4e1c-94ee-0bcf3bd9c6ef) - [Wikipédia](https://en.wikipedia.org/wiki/Dominating_set)
+:world_map: Dominant - on [codalab](https://competitions.codalab.org/competitions/35662?secret_key=d486de59-a936-4e1c-94ee-0bcf3bd9c6ef) - on [Wikipédia](https://en.wikipedia.org/wiki/Dominating_set)
 
 ```bash 
 python3 dominant.py rep_data_set rep_answer
--- dominant/dominant.py
+++ dominant/dominant.py
@@ -1,23 +1,68 @@
 import sys, os, time
 import networkx as nx
 
+def order_graph(g) :
+    """
+    sort the graph according to the succession of nodes
+    """
+    first_node = list(g.nodes)[0] 
+    new_graph = [first_node] 
+    new_graph.append(list(g[first_node])[0]) 
+    while len(new_graph) != g.order() :
+        adjacent = list(g[new_graph[-1]]) 
+        adjacent.remove(new_graph[-2]) 
+        new_graph.append(adjacent[0]) 
+    return new_graph
+
+def cycle_dominant(g):
+    dominant_set = set()
+    ordered_graph = order_graph(g)
+    for i in range(0, len(ordered_graph), 3):
+        dominant_set.add(ordered_graph[i])
+    return dominant_set
 
 def dominant(g):
     """
         A Faire:         
         - Ecrire une fonction qui retourne le dominant du graphe non dirigé g passé en parametre.
         - cette fonction doit retourner la liste des noeuds d'un petit dominant de g
-
         :param g: le graphe est donné dans le format networkx : https://networkx.github.io/documentation/stable/reference/classes/graph.html
-
     """
-    return g.nodes  # pas terrible :) mais c'est un dominant
-
+    all_nodes = set(g)
+    adjacent_nb = {} 
+    for node in all_nodes :
+        adjacent_nb[g.degree[node]] = node
+    if len(adjacent_nb) == 1 and (g.number_of_nodes() == g.number_of_edges()) : 
+        return cycle_dominant(g)
+    else :
+        max_adjacent = max(adjacent_nb.keys()) 
+        max_node = adjacent_nb[max_adjacent]        
+        dominating_set = {max_node}
+        not_selected = all_nodes - {max_node} 
+        all_nodes = all_nodes - set(g[max_node]) - {max_node} 
+        g = g.subgraph(not_selected) 
+        while all_nodes :
+            adjacent_nb = {} 
+            for node in not_selected :
+                remaining_adjacent = []
+                for node2 in list(g[node]) :
+                    if node2 in all_nodes :
+                        remaining_adjacent.append(node2)
+                adjacent_nb[len(remaining_adjacent)] = node
+            if len(adjacent_nb) == 1 and (g.number_of_nodes() == g.number_of_edges()) :
+                dominating_set |= cycle_dominant(g)
+                return dominating_set
+            max_adjacent = max(adjacent_nb.keys())
+            max_node = adjacent_nb[max_adjacent]
+            dominating_set.add(max_node)
+            not_selected = not_selected - {max_node}
+            all_nodes = all_nodes - set(g[max_node]) - {max_node}
+            g = g.subgraph(not_selected)
+    return dominating_set
 
 #########################################
 #### Ne pas modifier le code suivant ####
 #########################################
-
 
 def load_graph(name):
     with open(name, "r") as f:
@@ -79,7 +124,7 @@ if __name__ == "__main__":
         print(input_dir, "doesn't exist")
         exit()
 
-        # fichier des reponses depose dans le output_dir et annote par date/heure
+    # fichier des reponses depose dans le output_dir et annote par date/heure
     output_filename = 'answers_{}.txt'.format(time.strftime("%d%b%Y_%H%M%S", time.localtime()))
     output_file = open(os.path.join(output_dir, output_filename), 'w')
 
